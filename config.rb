a453023c559a434d131bb256b800d0fe116a9038
= <=>
2014-03-26 17:45:25 +0100

    Added method to use bower with sprockets.

--- config.rb
+++ config.rb
@@ -42,6 +42,17 @@
   #set :css_dir, 'stylesheets'
   #set :images_dir, 'images'
 
+  # Add bower's directory to sprockets asset path
+  #after_configuration do
+    #@bower_config = JSON.parse(IO.read("#{root}/.bowerrc"))
+    #sprockets.append_path File.join "#{root}", @bower_config["directory"]
+  #end
+  after_configuration do
+    @bower_config = JSON.parse(IO.read("#{root}/.bowerrc"))
+    @bower_assets_path = File.join "#{root}", @bower_config["directory"]
+    sprockets.append_path @bower_assets_path
+    sprockets.append_path 'vendor/assets/assets/leaflet-dist'
+  end
   # Methods defined in the helpers block are available in templates
   # helpers do
   #   def some_helper
